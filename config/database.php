d4a65fd1a83f7d78d94e900e5279abde42583f37
peterjewicz <peterjewicz@totalwebconnections.com>
2017-05-17 18:21:32 -0500

    first commit

--- config/database.php
+++ config/database.php
@@ -39,19 +39,16 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+        'mysql'  => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
+            'database'  => env('DB_DATABASE', 'heroku_0da649ffb025375'),
+            'username'  => env('DB_USERNAME', 'b084e6a5368792'),
+            'password'  => env('DB_PASSWORD', 'ed51bce5'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
         ],
 
         'pgsql' => [
