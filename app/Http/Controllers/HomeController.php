e29b130abc428823e9ec521643fa8d13f97d4278
peterjewicz <peterjewicz@totalwebconnections.com>
2017-05-26 16:30:52 -0500

    mobile api call for home screen

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,4 +19,15 @@ class HomeController extends Controller
         $deal->images = explode ( ',' , $deal->images ); // break apart the images into an array for display loop
         return view('home')->with('deal', $deal);
     }
+
+    /**
+     * returns the most recent deal as
+     * a json string (used as api)
+     * @return json string
+     */
+    public function mobile(){
+        $deal = DB::table('deals')->orderBy('id', 'desc')->first();
+        $deal->images = explode ( ',' , $deal->images ); // break apart the images into an array for display loop
+        return json_encode($deal));
+    }
 }
